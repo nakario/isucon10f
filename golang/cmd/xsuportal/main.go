58f08fa4497ee204a0b32a959cd34fe2db370bec
aokabi <aokabi@gmail.com>
2020-10-05 16:21:18 +0900

    cache login

--- golang/cmd/xsuportal/main.go
+++ golang/cmd/xsuportal/main.go
@@ -50,6 +50,7 @@ const (
 
 var db *sqlx.DB
 var notifier xsuportal.Notifier
+var contestantIdMap map[string]string = make(map[string]string, 300)
 
 func main() {
 	go func() { log.Println(http.ListenAndServe(":9090", nil)) }()
@@ -179,7 +180,7 @@ func (*AdminService) Initialize(e echo.Context) error {
 	if req.Contest != nil {
 		freezesAt := req.Contest.ContestFreezesAt.AsTime().Round(time.Microsecond)
 		endsAt := req.Contest.ContestEndsAt.AsTime().Round(time.Microsecond)
-		go func(){
+		go func() {
 			<-time.After(freezesAt.Sub(time.Now()))
 			freezeCh <- struct{}{}
 			<-time.After(endsAt.Sub(time.Now()))
@@ -735,6 +736,8 @@ func (*ContestantService) Logout(e echo.Context) error {
 		if err := sess.Save(e.Request(), e.Response()); err != nil {
 			return fmt.Errorf("delete session: %w", err)
 		}
+		cookie, _ := e.Cookie(SessionName)
+		delete(contestantIdMap, cookie.Value)
 	} else {
 		return halt(e, http.StatusUnauthorized, "ログインしていません", nil)
 	}
@@ -1053,6 +1056,8 @@ func (*RegistrationService) DeleteRegistration(e echo.Context) error {
 	if err := tx.Commit(); err != nil {
 		return fmt.Errorf("commit tx: %w", err)
 	}
+	cookie, _ := e.Cookie(SessionName)
+	delete(contestantIdMap, cookie.Value)
 	return writeProto(e, http.StatusOK, &registrationpb.DeleteRegistrationResponse{})
 }
 
@@ -1198,6 +1203,15 @@ type loginRequiredOption struct {
 }
 
 func loginRequired(e echo.Context, db sqlx.Queryer, option *loginRequiredOption) (bool, error) {
+	cookie, err := e.Cookie(SessionName)
+	if err != nil {
+		return false, halt(e, http.StatusUnauthorized, "ログインが必要です", nil)
+	}
+	if !option.Lock {
+		if _, ok := contestantIdMap[cookie.Value]; ok {
+			return true, nil
+		}
+	}
 	contestant, err := getCurrentContestant(e, db, option.Lock)
 	if err != nil {
 		return false, fmt.Errorf("current contestant: %w", err)
@@ -1205,6 +1219,7 @@ func loginRequired(e echo.Context, db sqlx.Queryer, option *loginRequiredOption)
 	if contestant == nil {
 		return false, halt(e, http.StatusUnauthorized, "ログインが必要です", nil)
 	}
+
 	if option.Team {
 		t, err := getCurrentTeam(e, db, option.Lock)
 		if err != nil {
@@ -1214,6 +1229,7 @@ func loginRequired(e echo.Context, db sqlx.Queryer, option *loginRequiredOption)
 			return false, halt(e, http.StatusForbidden, "参加登録が必要です", nil)
 		}
 	}
+	contestantIdMap[cookie.Value] = contestant.ID
 	return true, nil
 }
 
